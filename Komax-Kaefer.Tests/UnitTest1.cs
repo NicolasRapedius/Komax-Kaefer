56910cec98e9025f912dfcfe53212411fd766f20
Rapedius, Nicolas <nicolas.rapedius@komaxgroup.com>
2025-11-04 15:08:32 +0100

    Initial commit: Komax-Kaefer v2 project

---
+++ Komax-Kaefer.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Komax_Kaefer.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
